b4c86d087b242b92c2e6e74aa43a166366b4b86b
Thomas <omarini.thomas@gmail.com>
2020-05-21 17:44:14 +0200

    pb unité

--- 211_Projet/211_Projet/main.py
+++ 211_Projet/211_Projet/main.py
@@ -12,10 +12,15 @@ import numpy as np
 # =====================
 
 # map(width, dx, k, m)
-width = 50
-dx = 1
-mass = 4/3*np.pi*dx**3 * 1.2e-3
-m = map(width, dx, 0.0005, mass)
+width = 1
+dx = 0.05    #mm
+rho = 1.2e-3 #kg/m**3
+mass = 4/3*np.pi*dx**3 * rho * 1e9
+k = 1 # mN/mm
+
+#ACCELERATION EN mili m/s**2
+
+m = map(width, dx, k, mass)
 sources = []
 
 mult = 1.0
@@ -60,8 +65,8 @@ fig_plan = plt.figure(figsize=[8, 8])
 ax = fig_plan.gca()
 
 #ax.grid(True)
-ax.set_xlim(-2*mult, (width+2)*mult)
-ax.set_ylim(-2*mult, (width+2)*mult)
+ax.set_xlim(-0.2*width*mult, (width*1.2)*mult)
+ax.set_ylim(-0.2*width*mult, (width*1.2)*mult)
 
 #c = ax.pcolor([[acoustic_level(point(x/mult, y/mult)) for x in map_range] for y in map_range], vmin=0, vmax=100, cmap='bwr')
 #ax.scatter(positions_x, positions_y, s=20)
@@ -76,19 +81,20 @@ source_scatter = ax.scatter([], [], s=20, c='r')
 b_src = m.sources[0]
 #b_src.p.x += 0.0005
 f = 1
-a = 0.5
+a = dx*0.225
 
 global temps 
 temps = 0
-watching_boul =  m.boules[int(len(m.boules)/2)][int(len(m.boules)/2)+5]
+watching_boul =  m.boules[int(len(m.boules)/2)][int(len(m.boules))-5]
 
 def velocity(dt):
     # Velocity of the wave    
     global temps
     
     temps += dt
-    if watching_boul.p.x-watching_boul.p_0.x >= a:
-        vitesse = sqrt((watching_boul.p.x - b_src.p.x)**2 + (watching_boul.p.y - b_src.p.y)**2)/temps
+    
+    if watching_boul.p - watching_boul.p_0 >=1e-12 :
+        vitesse = watching_boul.p_0.distance(b_src.p_0)/temps
         print("Wave velocity: {}".format(vitesse))
     
     
@@ -109,17 +115,17 @@ def animate(t):
             b_src.p.x += a*sin((t-start_time) * 2*pi*f)
         else:
             m.sources.clear()
+            velocity(dt)
         #b_src.p.y += a*sin(t * 2*pi*f)
 
         # Solve step time
         tstart = time()
-#        m.resolution(dt)
+        m.resolution(dt)
         tstop = time()
         
         avg_ctime += tstop - tstart
         cpt += 1
         
-        velocity(dt)
         
     boule_scatter.set_offsets(np.c_[[b.p.x for (i, j), b in np.ndenumerate(m.boules)], [b.p.y for (i, j), b in np.ndenumerate(m.boules)]])
     source_scatter.set_offsets(np.c_[[b.p.x for b in m.sources], [b.p.y for b in m.sources]])
