736199be9e11ca923909f29e4ffc529ae5e96017
Thomas <omarini.thomas@gmail.com>
2020-05-23 17:28:47 +0200

    gradient descent 2

--- 211_Projet/211_Projet/main.py
+++ 211_Projet/211_Projet/main.py
@@ -9,206 +9,232 @@ from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 
-value_v_dx_k = []
+display = False
+verbose = True
+deferred_mode = True
 
-for compteur_k in range(1,25):  
-    k = 0.001+0.001*compteur_k  
-    for compteur_dx in range(1,25):
-        dx = 0.31+0.01*compteur_dx
-        verbose = True
-        deferred_mode = True
+# Variables & Constants
+# =====================
+
+# Geometry properties
+room_width = 10            # m
+
+# Physical properties
+dx = 0.35                   # m
+rho = 1.2e-3               # kg/m**3
+k = 0.001                 # mN/m
+mass = dx**3 * rho         # ng
+
+# ACCELERATION = m/s**2 * 1e-12
+
+# Map properties
+m = Map(room_width, dx, k, mass)
+color_mult = 1.0
+colormap_range = np.arange(0, room_width*color_mult)
+
+# Time properties
+start_time = 1             # Time to start simulation [s]
+stop_time = 2              # Time to stop simulation  [s]
+time_speed = 0.001             # Time speed multiplier    [1]
+fps = 60                   # Frames per second        [frame/s]
+dt = time_speed/fps        # Time interval            [s]
+
+times = np.arange(0, stop_time, dt)
+
+
+
+
+# Model & Resolution
+# ==================
+global speed
+
+watching_particle = m.particles[len(m.particles) // 2][len(m.particles) - 5]
+def compute_velocity(dt):
+    """Compute velocity of the wave"""
+    global speed
+    
+    if 't' not in compute_velocity.__dict__: compute_velocity.t = 0
+    if 'done' not in compute_velocity.__dict__: compute_velocity.done = False
+
+    compute_velocity.t += dt; tolerance = 1e-10
+    if watching_particle.p.distance(watching_particle.p_0) >= tolerance and not compute_velocity.done:
+        speed = watching_particle.p_0.distance(b_src.particle.p_0) / compute_velocity.t
+        compute_velocity.done = True
         
-        # Variables & Constants
-        # =====================
+global avg_ctime; avg_ctime = 0
+global cpt; cpt = 0
+particles_history = []
+sources_history = []
+
+
+def compute_all():
+    print_progress_bar(0, times[-1], prefix = 'Compute progress:', suffix = 'Complete', length = 50)
+    for t in times:
+        if t >= start_time:
+            compute_step(t)
+            print_progress_bar(t, times[-1], prefix = 'Compute progress:', suffix = 'Complete', length = 50)
+
+def compute_step(t):
+    global avg_ctime
+    global cpt
+
+    # Get next source state
+    if t < start_time + 0.5/f:
+        b_src.particle.p.x = b_src.particle.p_0.x + a/2*(1 + sin((t-start_time) * 2*pi*f))
+    else:
+        compute_velocity(dt)
+        m.clear_sources()
+
+    # Solve step time
+    tstart = time()
+    m.resolution(dt)
+    tstop = time()
+
+    # Add frame to history
+    if deferred_mode and display:
+        particles_history.append(deepcopy(m.particles))
+        sources_history.append(deepcopy(m.sources))
         
-        # Geometry properties
-        room_width = 10            # m
+    # Update average time
+    avg_ctime += tstop - tstart
+    cpt += 1
+
+def get_speed(k,dx):
+    global speed
+    
+    m = Map(room_width, dx, k, mass)
+    compute_all()
+    return speed
+    
+    
+    
+# Problem optimization
+# ====================
+
+m.add_source(room_width/2, room_width/2, 90)
+b_src = m.sources[0]
+
+f = 1
+a = 0.6125
+
+def cost(k, dx):
+    return (get_speed(k,dx)-340)**2
+
+step_k = 0.001
+step_dx = 0.001
+
+def partial_derivatives(k, dx):
+    new_k = k + step_k
+    error_k = cost(k,dx)
+    new_error_k = cost(new_k,dx)
+    derivate_cost_k = (new_error_k- error_k)/step_k
+    
+    new_dx = dx + step_dx
+    error_dx = cost(k,dx)
+    new_error_dx = cost(new_dx,dx)
+    derivate_cost_dx = (new_error_dx- error_dx)/step_dx
+    
+    return derivate_cost_k, derivate_cost_dx
+    
+def new_parameters(last_k, last_dx):
+    [derivate_cost_k, derivate_cost_dx] = partial_derivatives(k,dx)
+    new_k =  k - (learning_rate * derivate_cost_k)
+    new_dx = dx - (learning_rate * derivate_cost_dx)
+    return [new_k,new_dx]
+
+learning_rate = float(0.0001)
+initial_k = 0.001
+initial_dx = 0.1
+nber_iterations = 100
+ 
+def gradient_descent():
+    temporary_k = initial_k
+    temporary_dx = initial_dx
+    for i in range(nber_iterations):
+        [new_k, new_dx] = new_parameters(temporary_k, temporary_dx)
+        temporary_k = new_k
+        temporary_dx = new_dx
+    return [temporary_k, temporary_dx]         
+ 
+[final_k, final_dx] = gradient_descent()
+ 
+print("After {0} iterations k = {1} and dx = {2}".format(nber_iterations, final_k, final_dx))
+    
+
+# Solutions display
+# =================
+
+if display:
+    # Create a new figure
+    fig_plan = plt.figure(figsize=[8, 8])
+    ax = fig_plan.gca()
+    
+    # Set figure limits and properties
+    ax.set_xlim(-0.2*room_width*color_mult, 1.2*room_width*color_mult)
+    ax.set_ylim(-0.2*room_width*color_mult, 1.2*room_width*color_mult)
+    
+    # Create scatter plots
+    particles_scatter = ax.scatter([], [], s=10, marker='s')
+    source_scatter = ax.scatter([], [], s=15, marker='s', c='r')
+    
+    #c = ax.pcolor([[acoustic_level(point(x/mult, y/mult)) for x in map_range] for y in map_range], vmin=0, vmax=100, cmap='bwr')
+    #ax.scatter(positions_x, positions_y, s=20)
+    #fig_plan.colorbar(c, ax=ax)
+
+    # Figure animation live
+    def animate_live(t):
+        global avg_ctime
+        global cpt
         
-        # Physical properties
-        #dx = 0.35                   # m
-        rho = 1.2e-3               # kg/m**3
-        #k = 0.001                 # mN/m
-        mass = dx**3 * rho         # ng
-        
-        # ACCELERATION = m/s**2 * 1e-12
-        
-        # Map properties
-        m = Map(room_width, dx, k, mass)
-        color_mult = 1.0
-        colormap_range = np.arange(0, room_width*color_mult)
-        
-        # Time properties
-        start_time = 1             # Time to start simulation [s]
-        stop_time = 2              # Time to stop simulation  [s]
-        time_speed = 1             # Time speed multiplier    [1]
-        fps = 1200                   # Frames per second        [frame/s]
-        dt = time_speed/fps        # Time interval            [s]
-        
-        times = np.arange(0, stop_time, dt)
-        
-        # Model & Resolution
-        # ==================
-        global speed
-        
-        watching_particle = m.particles[len(m.particles) // 2][len(m.particles) - 5]
-        def compute_velocity(dt):
-            """Compute velocity of the wave"""
-            global speed
-            
-            if 't' not in compute_velocity.__dict__: compute_velocity.t = 0
-            if 'done' not in compute_velocity.__dict__: compute_velocity.done = False
-        
-            compute_velocity.t += dt; tolerance = 1e-10
-            if watching_particle.p.distance(watching_particle.p_0) >= tolerance and not compute_velocity.done:
-                speed = watching_particle.p_0.distance(b_src.particle.p_0) / compute_velocity.t
-                compute_velocity.done = True
-        
-        # Problem optimization
-        # ====================
-        
-        m.add_source(room_width/2, room_width/2, 90)
-        b_src = m.sources[0]
-        
-        f = 1
-        a = 0.6125
-        
-        # Solutions display
-        # =================
-        
-        # Create a new figure
-        fig_plan = plt.figure(figsize=[8, 8])
-        ax = fig_plan.gca()
-        
-        # Set figure limits and properties
-        ax.set_xlim(-0.2*room_width*color_mult, 1.2*room_width*color_mult)
-        ax.set_ylim(-0.2*room_width*color_mult, 1.2*room_width*color_mult)
-        
-        # Create scatter plots
-        particles_scatter = ax.scatter([], [], s=10, marker='s')
-        source_scatter = ax.scatter([], [], s=15, marker='s', c='r')
-        
-        #c = ax.pcolor([[acoustic_level(point(x/mult, y/mult)) for x in map_range] for y in map_range], vmin=0, vmax=100, cmap='bwr')
-        #ax.scatter(positions_x, positions_y, s=20)
-        #fig_plan.colorbar(c, ax=ax)
-        
-        
-        def partial_derivatives(last_k, last_dx):
-            pass
-        #    k_derivative = last_k + last_dx - 
-        #    dx_derivative = 
-            
-        def new_parameters(last_k, last_dx):
-            [derivee_theta_0, derivee_theta_1] = partial_derivatives(last_k,last_dx)
-            new_k = last_k - (learning_rate_ALPHA * k_derivative)
-            new_dx = last_dx - (learning_rate_ALPHA * dx_derivative)
-            return [new_k,new_dx]
-        
-        learning_rate_ALPHA = float(0.0001)
-        initial_k = float(0)
-        initial_dx = float(0)
-        nber_iterations = 100
-         
-        def gradient_descent():
-            temporary_k = initial_k
-            temporary_dx = initial_dx
-            for i in range(nber_iterations):
-                [new_k, new_dx] = new_parameters(temporary_k, temporary_dx)
-                temporary_k = new_k
-                temporary_dx = new_dx
-            return [temporary_k, temporary_dx]         
-         
-        #[final_k, final_dx] = gradient_descent()
-        # 
-        #print "After {0} iterations k = {1} and dx = {2}".format(nber_iterations, final_k, final_dx)
-        
-            
-        global avg_ctime; avg_ctime = 0
-        global cpt; cpt = 0
-        particles_history = []
-        sources_history = []
-        
-        def compute_all():
-            print_progress_bar(0, times[-1], prefix = 'Compute progress:', suffix = 'Complete', length = 50)
-            for t in times:
-                if t >= start_time:
-                    compute_step(t)
-                    print_progress_bar(t, times[-1], prefix = 'Compute progress:', suffix = 'Complete', length = 50)
-        
-        def compute_step(t):
-            global avg_ctime
-            global cpt
-        
-            # Get next source state
-            if t < start_time + 0.5/f:
-                b_src.particle.p.x = b_src.particle.p_0.x + a/2*(1 + sin((t-start_time) * 2*pi*f))
-            else:
-                compute_velocity(dt)
-                m.clear_sources()
-        
-            # Solve step time
-            tstart = time()
-            m.resolution(dt)
-            tstop = time()
-        
-            # Add frame to history
-            if deferred_mode:
-                particles_history.append(deepcopy(m.particles))
-                sources_history.append(deepcopy(m.sources))
-                
-            # Update average time
-            avg_ctime += tstop - tstart
-            cpt += 1
-        
-        # Figure animation live
-        def animate_live(t):
-            global avg_ctime
-            global cpt
-            
-            if t >= start_time:
-                compute_step(t)
-        
-            # Update all points positions
-            particles_scatter.set_offsets(np.c_[[b.p.x for (i, j), b in np.ndenumerate(m.particles)], [b.p.y for (i, j), b in np.ndenumerate(m.particles)]])
-            source_scatter.set_offsets(np.c_[[src.particle.p.x for src in m.sources], [src.particle.p.y for src in m.sources]])
-        
-            # Return everything that must be updated
-            return (particles_scatter, source_scatter)
-        
-        # Figure animation deferred
-        def animate_deferred(t):
-            global avg_ctime
-            global cpt
-        
-            # Update all points positions
-            particles_scatter.set_offsets(np.c_[[b.p.x for (i, j), b in np.ndenumerate(particles_history[t])], [b.p.y for (i, j), b in np.ndenumerate(particles_history[t])]])
-            source_scatter.set_offsets(np.c_[[src.particle.p.x for src in sources_history[t]], [src.particle.p.y for src in sources_history[t]]])
-        
-            # Return everything that must be updated
-            return (particles_scatter, source_scatter)
-        
-        if deferred_mode:
-            compute_all()
-            # Configure animation with blit and repeating
-            anim = FuncAnimation(fig_plan, animate_deferred, frames=range(len(particles_history)), interval = 1000//fps, blit=True, repeat=True, repeat_delay=1000)
-        else:
-            # Configure animation with blit and not repeating
-            anim = FuncAnimation(fig_plan, animate_live, frames=times, interval = 1000//fps, blit=True, repeat=False)
-        
-        # Show the plot to the screen
-        #plt.show()
-        
-        # Show debug outputs
-        if verbose:
-            avg_ctime /= cpt
-            #print('Average step compute time: {} s'.format(avg_ctime))
-            
-        #Show velocity
-        print("Wave velocity: {0}, dx: {1}, k: {3}".format(speed,dx,k)) 
-        
-        value_v_dx_k += [speed,dx,k]
-        
-        
-        
-        
-        
-        
+        if t >= start_time:
+            compute_step(t)
+    
+        # Update all points positions
+        particles_scatter.set_offsets(np.c_[[b.p.x for (i, j), b in np.ndenumerate(m.particles)], [b.p.y for (i, j), b in np.ndenumerate(m.particles)]])
+        source_scatter.set_offsets(np.c_[[src.particle.p.x for src in m.sources], [src.particle.p.y for src in m.sources]])
+    
+        # Return everything that must be updated
+        return (particles_scatter, source_scatter)
+    
+    # Figure animation deferred
+    def animate_deferred(t):
+        global avg_ctime
+        global cpt
+    
+        # Update all points positions
+        particles_scatter.set_offsets(np.c_[[b.p.x for (i, j), b in np.ndenumerate(particles_history[t])], [b.p.y for (i, j), b in np.ndenumerate(particles_history[t])]])
+        source_scatter.set_offsets(np.c_[[src.particle.p.x for src in sources_history[t]], [src.particle.p.y for src in sources_history[t]]])
+    
+        # Return everything that must be updated
+        return (particles_scatter, source_scatter)
+    
+    if deferred_mode:
+        compute_all()
+        # Configure animation with blit and repeating
+        anim = FuncAnimation(fig_plan, animate_deferred, frames=range(len(particles_history)), interval = 1000//fps, blit=True, repeat=True, repeat_delay=1000)
+    else:
+        # Configure animation with blit and not repeating
+        anim = FuncAnimation(fig_plan, animate_live, frames=times, interval = 1000//fps, blit=True, repeat=False)
+    
+    # Show the plot to the screen
+    plt.show()
+
+
+    
+
+
+
+
+# Show debug outputs
+if verbose:
+    avg_ctime /= cpt
+    #print('Average step compute time: {} s'.format(avg_ctime))
+    
+#Show velocity
+print("Wave velocity: {0}, dx: {1}, k: {3}".format(speed,dx,k)) 
+
+
+
+
+
+
